f7b031069655979cf4d4fc4b3cb6d79a260b1aa6
SaasCrm01 <saascrm01@gmail.com>
2024-07-12 15:06:53 -0400

    att nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Navbar from './components/Navbar';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -22,7 +22,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <div className="d-flex">
-          <Navbar />
+          
           <main className="flex-grow-1 p-4">{children}</main>
         </div>
       </body>
